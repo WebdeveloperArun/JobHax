ec7e0da9a6f36b705b4fcf59afe422aadea7581b
WebDeveloperArun <forwebdeveloper0@gmail.com>
2026-04-06 15:55:16 +0530

    Check user already exists

--- app/register/registrationAction.action.ts
+++ app/register/registrationAction.action.ts
@@ -3,6 +3,7 @@
 import { db } from "@/src/config/db";
 import { users } from "@/src/drizzle/schema";
 import argon2 from "argon2"
+import { eq, or } from "drizzle-orm";
 
 
 export const registrationAction = async (data: {
@@ -14,6 +15,17 @@ export const registrationAction = async (data: {
 }) => {
     try {
         const { name, userName, email, password, role } = data;
+        const [user] = await db.select().from(users).where(or(eq(users.email, email), eq(users.userName, userName)));
+        if (user) {
+            if (user.email === email) return {
+                status: "ERROR",
+                message: "Email already exists"
+            }
+            else return {
+                status: "ERROR",
+                message: "Username already exists"
+            }
+        }
         const hashedPassword = await argon2.hash(password);
         await db.insert(users).values({ name, userName, email, password: hashedPassword, role });
         return {
